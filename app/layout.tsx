784ae450c0ec7bdb7c0b928e71aa41197416ab4c
MEDBOUAYOUNE <bouayounemoha1@gmail.com>
2025-07-11 14:46:07 +0200

    add favicon and update terms page still need to fix table of contents bugs

--- app/layout.tsx
+++ app/layout.tsx
@@ -7,10 +7,14 @@ import MinimalFooter from "./components/minimal-footer"
 const inter = Inter({ subsets: ["latin"] })
 
 export const metadata: Metadata = {
-  title: "JOTIQ - AI-Powered Business Development Tool",
+  title: "JOTIQ | AI-Powered Business Development Tool",
   description:
     "Automate client outreach, find new clients, and save hours every day with JOTIQ's AI-powered business development tools built for recruiters and global businesses.",
   keywords: "AI outreach, cold calling, lead generation, business development, recruiting tools",
+  icons: {
+    icon: "/favicon.png",
+    // apple: "/apple-touch-icon.png",
+  },
 }
 
 export default function RootLayout({
